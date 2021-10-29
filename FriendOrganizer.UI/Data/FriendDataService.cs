71512191f2d4b38339ba0bd49384b4fb9544c863
Scott Lassiter <slassiter@scholarshipamerica.org>
2021-10-29 16:26:05 -0500

    Added friend organizer files from work laptop

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,18 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load Data later
+            yield return new Friend { FirstName = "Bray", LastName = "Calvin" };
+            yield return new Friend { FirstName = "Neenah", LastName = "Stone" };
+            yield return new Friend { FirstName = "Maya", LastName = "Gene" };
+            yield return new Friend { FirstName = "Max", LastName = "Catty" };
+
+        }
+    }
+}
